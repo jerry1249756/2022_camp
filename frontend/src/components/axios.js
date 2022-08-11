adb00faba0034657dee78f3dface597c94d74301
jerry1249756 <jerry1249756@gmail.com>
2022-08-11 17:25:19 +0800

    change api

--- frontend/src/components/axios.js
+++ frontend/src/components/axios.js
@@ -1,7 +1,9 @@
 import _axios from "axios";
 
 const instance = _axios.create({
-  baseURL: "http://localhost:4000/api",
+  //baseURL: "http://localhost:4000/api", //local test
+  baseURL: "http://localhost:5000/api", //local heroku
+  //baseURL: "https://ntuee-monopoly.herokuapp.com/api" //deploy heroku
   timeout: 1000,
 });
 
